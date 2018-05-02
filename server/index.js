b430d9f16652df511b015a2679e82ac32a301833
kkxxll <550893685@qq.com>
2018-05-02 14:28:17 +0800

    init

--- server/index.js
+++ server/index.js
@@ -1,25 +1,7 @@
 const Koa = require('koa')
-const logger = require('koa-logger')
 const app = new Koa()
-
-const mid1 = async(ctx, next) => {
-    ctx.body = 'hello,'
-    await next()
-    ctx.body = ctx.body + 'after'
-}
-const mid2 = async(ctx, next) => {
-    ctx.type = 'text/html; charset=utf-8'
-    await next()
-}
-const mid3 = async(ctx, next) => {
-    ctx.body = ctx.body + 'world!!'
-    await next()
-}
-app.use(logger())
-app.use(mid1)
-app.use(mid2)
-app.use(mid3)
-
-// 注意执行结果是 hello world after
+app.use(async (ctx, next) => {
+    ctx.body = '首页'
+})
 
 app.listen(2333)
