1490d4d83f67ffc74b555db04f4df7a5e291873c
kkxxll <550893685@qq.com>
2018-05-03 14:44:38 +0800

    七牛云和事件循环

---
+++ server/config/index.js
@@ -0,0 +1,8 @@
+module.exports = {
+  "qiniu": {
+    "bucket": "douban",
+    "video": "p84wum00s.bkt.clouddn.com",
+    "AK": "zawfidZKXEUqH6MqzQpOzoZiiyILu90O7rQC40E5",
+    "SK": "KuND43gZYHXzodVSNzJ6kFXFOYmhm4TBPwU5KEcm"
+  }
+}
